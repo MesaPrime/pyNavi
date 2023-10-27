c0d48ec22959d440f1bca915ffd1d87e97a642c4
MesaPrime <36322644+MesaPrime@users.noreply.github.com>
2023-10-27 15:59:51 +0800

    commit

--- moudle/download.py
+++ moudle/download.py
@@ -15,11 +15,10 @@ origin: geopandas.geodataframe.GeoDataFrame = gdf_nodes.sample()
 desi: geopandas.geodataframe.GeoDataFrame = gdf_nodes.sample()
 
 path = osmnx.routing.k_shortest_paths(mapMultiGraph, origin.iloc[0].name, desi.iloc[0].name, 1)
-l = list()
 for _ in path:
-    l.append(_)
-length = osmnx.utils_graph.route_to_gdf(mapMultiGraph, l)["length"]
-print(sum(length))
+    length = osmnx.utils_graph.route_to_gdf(mapMultiGraph, _)['length']
+    print(_)
+    print(sum(length))
 
 
 def downloadFile(area: Optional[list], ):
