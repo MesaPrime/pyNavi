dd44783185611059352d6619c218d9cc7d2f0e8b
MesaPrime <36322644+MesaPrime@users.noreply.github.com>
2023-12-22 16:10:45 +0800

    commit

--- test/apiTest.py
+++ test/apiTest.py
@@ -1,8 +1,14 @@
 import httpx
+import asyncio
+import pytest
+
+pytest_plugins = ('pytest_asyncio',)
 
 headers = {'Authorization': 'Basic q25289577@outlook.com:pass'}
 
 
-def test_asyncHttp():
-    with httpx.AsyncClient() as client:
-        req = client.post()
+@pytest.mark.asyncio
+async def test_asyncHttp():
+    async with httpx.AsyncClient(verify=False) as client:
+        req = await client.get('https://master.apis.dev.openstreetmap.org.json')
+        print(req.json())
