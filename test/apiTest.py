fe438f4d69a997ed2d901a1f237393cad94fa668
MesaPrime <36322644+MesaPrime@users.noreply.github.com>
2023-12-22 20:08:06 +0800

    commit

--- test/apiTest.py
+++ test/apiTest.py
@@ -1,14 +1,20 @@
 import httpx
 import asyncio
 import pytest
+import xmltodict
+import json
 
 pytest_plugins = ('pytest_asyncio',)
-
-headers = {'Authorization': 'Basic q25289577@outlook.com:pass'}
+with open(r'../moudle/osmAuth.json') as file:
+    headerFromFile = json.loads(file.read())
+headers = httpx.Headers(headerFromFile)
 
 
 @pytest.mark.asyncio
-async def test_asyncHttp():
-    async with httpx.AsyncClient(verify=False) as client:
-        req = await client.get('https://master.apis.dev.openstreetmap.org.json')
+async def asyncHttp_test():
+    async with httpx.AsyncClient() as client:
+        req = await client.get('https://master.apis.dev.openstreetmap.org/api/capabilities.json', headers=headers)
         print(req.json())
+
+
+asyncio.run(asyncHttp_test())
